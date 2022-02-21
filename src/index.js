3d7a34bd0b247f3fd899544cc007e3ec99d8677b
UiguunaMikhailova <uyguuna1997@yandex.ru>
2022-02-21 17:04:20 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let size = 10
+  let array = []
+  for (let i = 0; i < expr.length; i += size) {
+    array.push(expr.slice(i, i + size))
+  }
+let arr = array.map((item) => {
+  let res = ''
+  if (item === '**********') {
+    res += ' '
+  } else {
+    item = String(+item)
+    for (let i = 0; i < item.length; i++) {
+      if (i % 2 !== 0) {
+        if (item[i] === '1') {
+          res += '-'
+        } else {
+          res += '.'
+        }
+      }
+    }
+}
+  return res
+})
+let string = ''
+for (let item of arr) {
+  if (item === ' ') {
+    string += ' '
+  } else {
+    string += MORSE_TABLE[item]
+  }
+}
+return string
 }
 
 module.exports = {
